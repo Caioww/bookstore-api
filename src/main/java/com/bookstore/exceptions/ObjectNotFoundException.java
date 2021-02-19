077f8c589771f13231e2263389e70b42b9d1befd
Caioww <sousinhacaio@gmail.com>
2021-02-19 01:10:09 -0300

    exceção personalizada objeto não encontrado

---
+++ src/main/java/com/bookstore/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,21 @@
+package com.bookstore.exceptions;
+
+
+public class ObjectNotFoundException  extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1085017306867567172L;
+
+	public ObjectNotFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ObjectNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
--- src/main/java/com/bookstore/service/CategoriaService.java
+++ src/main/java/com/bookstore/service/CategoriaService.java
@@ -6,20 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.Categoria;
+import com.bookstore.exceptions.ObjectNotFoundException;
 import com.bookstore.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria categoria;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		
-		return obj.orElse(null);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
