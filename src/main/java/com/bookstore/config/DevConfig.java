f6647627367d21cca40bba36e22e88452cc4ef2d
Caioww <sousinhacaio@gmail.com>
2021-02-19 00:20:40 -0300

    criação perfil de DEV + Config mysql

---
+++ src/main/java/com/bookstore/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bookstore.service.DBService;
+
+@Configuration
+@Profile(value = { "dev" })
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean init() {
+		
+		if(strategy.equals("create") ) {
+			this.dbService.iniciarBaseDeDados();
+		}
+		
+		return false;
+		
+	}
+
+}
